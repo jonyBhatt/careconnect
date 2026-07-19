1cf676f6c272960040797a3c41f17285ee2e4cba
jonyBhatt <jonybhattacharjee886@gmail.com>
2026-07-19 15:46:08 +0600

    Project initialize

---
+++ src/lib/prisma.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { PrismaClient } from "../../generated/prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents, no-var
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
