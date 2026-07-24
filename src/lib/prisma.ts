2a07fd7f96ad499cb16916cd195becbda86f295b
jonyBhatt <jonybhattacharjee886@gmail.com>
2026-07-24 15:28:58 +0600

    updated prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,6 @@
+import "dotenv/config";
 import { PrismaClient } from "../../generated/prisma/client";
 
 const prisma = new PrismaClient();
 
-const globalForPrisma = global as unknown as { prisma: typeof prisma };
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
+export { prisma };
