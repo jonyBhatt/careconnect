9ed1ace0b9a8a61a03bb751efd6cc2d82b3434ca
jonyBhatt <jonybhattacharjee886@gmail.com>
2026-07-25 02:57:25 +0600

    Update folder structure

--- src/app/(root)/(web)/layout.tsx
+++ src/app/(root)/(web)/layout.tsx
@@ -1,3 +1,16 @@
-export default function WebLayout({ children }: { children: React.ReactNode }) {
-  return <section>{children}</section>;
+import { Navbar } from "@/components/shared/Navbar";
+import { Footer } from "@/components/shared/Footer";
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section className="min-h-dvh flex flex-col w-full bg-white">
+      <Navbar />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </section>
+  );
 }
