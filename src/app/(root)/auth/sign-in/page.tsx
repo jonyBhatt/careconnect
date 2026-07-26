5794dd95f608d646d8ffce2a1e0eee815902f57c
jonyBhatt <jonybhattacharjee886@gmail.com>
2026-07-26 06:41:17 +0600

    typo

--- src/app/(root)/auth/sign-in/page.tsx
+++ src/app/(root)/auth/sign-in/page.tsx
@@ -1,22 +1,41 @@
 import SigninForm from "@/components/form/SigninForm";
+import Link from "next/link";
 
 export default function SigninPage() {
-  return (
-    <div className="h-screen bg-linear-to-b from-teal-600/40 via-blue-300/30 to-gray-400/20 font-sans">
-      <div className="px-4 sm:px-8 lg:px-12 flex flex-col py-16">
-        <div className="flex flex-col gap-6">
-          <h2 className="font-semibold font-sans text-4xl">
-            Welcome to Careconnect
-          </h2>
-          <p className="text-lg text-muted-foreground font-normal">
-            Please enter your phone number to <br /> receive a secure
-            verification code.
-          </p>
+    return (
+        <div className="h-screen bg-linear-to-b from-teal-600/40 via-blue-300/30 to-gray-400/20 font-sans">
+            <div className="px-4 sm:px-8 lg:px-12 flex flex-col py-16">
+                <div className="flex flex-col gap-6">
+                    <h2 className="font-semibold font-sans text-4xl">
+                        Welcome to Careconnect
+                    </h2>
+                    <p className="text-lg text-muted-foreground font-normal">
+                        Please enter your phone number to <br/> receive a secure
+                        verification code.
+                    </p>
+                </div>
+                <div className="rounded-md  my-5">
+                    <SigninForm/>
+                </div>
+                <div className={"p-6 bg-gray-300/40 rounded-xl flex flex-col gap-2"}>
+                    <h2 className={"font-medium text-lg"}>
+                        Secure Connection
+                    </h2>
+                    <span className={"text-muted-foreground font-light"}>
+                        Your health data is encrypted and <br/> protected under HIPPA <br/> regulations
+                     </span>
+                </div>
+                <div className={"flex flex-col gap-2 my-5"}>
+                    <p className={"text-lg font-medium text-muted-foreground"}>
+                        New to CareConnect?
+                    </p>
+                    <Link href={"/auth/sign-up"}>
+                        <h4 className={"font-semibold text-lg text-blue-600"}>
+                            Create Account
+                        </h4>
+                    </Link>
+                </div>
+            </div>
         </div>
-        <div className="rounded-md  my-5">
-          <SigninForm />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
